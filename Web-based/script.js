8cf97cd123c3bb3dadb4e8d1765c24bea3c7bf4c
Ohela <153936934+alohaasparo@users.noreply.github.com>
2025-11-29 20:38:45 +0800

    Add files via upload

---
+++ Web-based/script.js
@@ -0,0 +1,23 @@
+//PROGRAMMS
+//BUTTON 1
+const Btn1 = document.getElementById('Btn1');
+Btn1.addEventListener ("click", function () {
+    window.location.href = "LearnProg.html";
+});
+//BUTTON 2
+const Btn2 = document.getElementById('Btn2');
+Btn2.addEventListener ("click", function() {
+    window.location.href = "LearnProgLanguages.html";
+});
+
+//LEARN PROG LANGUAGES BUTTONS
+//BEGINNERS BTN
+const beginnersbtn = document.getElementById('beginnersbtn');
+beginnersbtn.addEventListener ("click", function() {
+    window.location.href = "ForBeginners.html";
+});
+const WebDevbtn = document.getElementById('WebDevbtn');
+WebDevbtn.addEventListener ("click", function() {
+    window.location.href = "LearnProgLanguages.html";
+});
+
