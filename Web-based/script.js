fcd2399144fc4a1ba21328f92a87b230be46d392
Ohela <153936934+alohaasparo@users.noreply.github.com>
2025-12-02 01:06:49 +0800

    Add files via upload

---
+++ Web-based/script.js
@@ -0,0 +1,51 @@
+// PROGRAMMS PAGE BUTTONS
+//BUTTON 1
+const Btn1 = document.getElementById('Btn1');
+if (Btn1) {
+    Btn1.addEventListener("click", function () {
+        window.location.href = "LearnProg.html";
+    });
+}
+//BUTTON 2
+const Btn2 = document.getElementById('Btn2');
+if (Btn2) {
+    Btn2.addEventListener("click", function () {
+        window.location.href = "LearnProgLanguages.html";
+    });
+}
+//BUTTON 2
+const Btn3 = document.getElementById('Btn3');
+if (Btn3) {
+    Btn3.addEventListener("click", function () {
+        window.location.href = "GAMES.html";
+    });
+}
+
+
+// LEARN PROGRAM LANGUAGES PAGE BUTTONS
+const beginnersbtn = document.getElementById('beginnersbtn');
+if (beginnersbtn) {
+    beginnersbtn.addEventListener("click", function () {
+        window.location.href = "ForBeginners.html";
+    });
+}
+//HTML
+const htmlbtn = document.getElementById('htmlbtn');
+if (htmlbtn) {
+    htmlbtn.addEventListener("click", function () {
+        window.location.href = "HTML.html";
+    });
+}
+
+
+
+
+
+
+//WEB DEVELOPMENT
+const WebDevbtn = document.getElementById('WebDevbtn');
+if (WebDevbtn) {
+    WebDevbtn.addEventListener("click", function () {
+        window.location.href = "LearnProgLanguages.html";
+    });
+}
